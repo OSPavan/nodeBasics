39f040b56df79a9d782dc33e1897b561ab504ebf
Pavan OS <pavan.os@mresult.com>
2023-04-11 21:07:28 +0530

    post api

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const http = require("http")
+const fs = require('fs')
 
-const server = http.createServer((req,res )=>{
-   res.write("hello");
-   res.end();
+const server = http.createServer((req, res) => {
+   let url = req.url
+   let method = req.method
+   if (url == '/') {
+      res.write("<html><h1>Enter Message</h1><form action='/postMessage' method ='POST'><input type='text' name = 'inputMessage'><button type='submit'>Submit</button></form></html>")
+      return res.end()
+   }
+   if(url == '/postMessage' && method == 'POST'){
+      fs.writeFileSync('message.txt','From node JS!')
+      // console.log(req);
+      res.statusCode = 302;   
+      // 302 is for redirect status
+      res.setHeader('Location','/')
+      return res.end()
+
+   }
+
 })
 
-server.listen(3000,()=>{
-console.log("server runing on 3000");
+
+
+
+server.listen(3000, () => {
+   console.log("server runing on 3000");
 })
